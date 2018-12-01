bc091ad37629d347280b876546c89ac1d3017771
Steven McCoy <steven@codevelop.ca>
2018-12-01 11:09:10 -0500

    cleanup

--- run.sh
+++ run.sh
@@ -6,13 +6,7 @@ if [ "${S3_S3V4}" = "yes" ]; then
     aws configure set default.s3.signature_version s3v4
 fi
 
-echo "-----------------------------"
-echo ${S3_BUCKET} : ${S3_PREFIX}
-echo "-----------------------------"
 
-echo "-----------------------------"
-echo "${CRON_SCHEDULE}"
-echo "-----------------------------"
 if [ "${CRON_SCHEDULE}" = "" ]; then
   sh backup.sh
 else
